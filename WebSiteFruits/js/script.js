78c36197d03630327c89631166119db426e64e2a
Gabriel Pereira <111321289+gabrielspereira1507@users.noreply.github.com>
2023-06-20 12:02:54 -0300

    Add files via upload

---
+++ WebSiteFruits/js/script.js
@@ -0,0 +1,15 @@
+function openwindow(){
+    document.getElementById("saibamais").style.display = "block";
+    document.getElementById("saibamais").style.width = "100vh"
+    document.getElementById("saibamais").style.height = "60vh"
+}
+function closewindow(){
+    document.getElementById("saibamais").style.display = "none";
+}function openwindow(){
+    document.getElementById("saibamais").style.display = "block";
+    document.getElementById("saibamais").style.width = "100vh"
+    document.getElementById("saibamais").style.height = "60vh"
+}
+function closewindow(){
+    document.getElementById("saibamais").style.display = "none";
+}
